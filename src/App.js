67488c2e6c85810a0b7ac5c9bbbd6bdeda196732
Akshay T K <akshayashok.tk@gmail.com>
2021-05-09 22:40:20 +0530

    browser route

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React from 'react' 
+import React from 'react'
 import './App.css';
-import {BrowserRouter,Route , Switch} from 'react-router-dom'
-import Home from './Home/home'      
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Home from './Home/home'
 import BuilderForm from './Builder/BuilderForm/builderForm'
 import ViewPdf from './View-Pdf/view-pdf'
 import { Provider } from "react-redux";
 import allReducers from '../src/Redux/store';
-import {createStore} from "redux";
-const store =createStore(allReducers)
+import { createStore } from "redux";
+const store = createStore(allReducers)
 
 
 function App() {
   return (
     <Provider store={store}>
-    <BrowserRouter basename={"https://akshay-tk.github.io/resume-builder-webapp"}>
-    <div className="App">
-     <Switch>
-       <Route path="/" component={Home} exact/>
-       <Route path="/resume-builder" component={BuilderForm}/>
-       <Route path="/view-pdf" component={ViewPdf}/>
-     </Switch>
-    </div>
-    </BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <div className="App">
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/resume-builder" component={BuilderForm} />
+            <Route path="/view-pdf" component={ViewPdf} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     </Provider>
   );
 }
